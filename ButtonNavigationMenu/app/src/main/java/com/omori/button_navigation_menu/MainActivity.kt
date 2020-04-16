44308791901375057b398aea4906f202d62cd2b1
ryosuke shimizu(清水 亮介) <RyShimizu@topcon.com>
2020-04-16 18:45:19 +0900

    #1 Finish ButtonNavigationMenu App.

--- ButtonNavigationMenu/app/src/main/java/com/omori/button_navigation_menu/MainActivity.kt
+++ ButtonNavigationMenu/app/src/main/java/com/omori/button_navigation_menu/MainActivity.kt
@@ -2,11 +2,71 @@ package com.omori.button_navigation_menu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var homeFragment: HomeFragment
+    lateinit var favFragment: FavFragment
+    lateinit var profileFragment: ProfileFragment
+    lateinit var settingsFragment: SettingsFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var buttomnav: BottomNavigationView = findViewById<BottomNavigationView>(R.id.button_nav_menu)
+        var frame: FrameLayout = findViewById<FrameLayout>(R.id.frame_layout)
+
+        buttomnav.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    homeFragment = HomeFragment()
+
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, homeFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+
+                R.id.favorite -> {
+                    favFragment = FavFragment()
+
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, favFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+
+                R.id.settings -> {
+                    settingsFragment = SettingsFragment()
+
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, settingsFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+
+                }
+
+                R.id.profile -> {
+                    profileFragment = ProfileFragment()
+
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, profileFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+
+                }
+            }
+
+            true
+        }
     }
 }
