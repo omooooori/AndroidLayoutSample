ae68479974298415458042e45c1cdecb7c9e51e1
ora1027 <r.shimizu18020@gmail.com>
2020-04-30 00:12:19 +0900

    Fix #17.

---
+++ CityGuideApp/app/src/main/java/com/omori/city_guide_app/UserDashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.omori.city_guide_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserDashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_dashboard)
+    }
+}
